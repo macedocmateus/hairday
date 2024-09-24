46475d16bc4a5cf1bdfb62391c2069fc8745d1d5
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-09-24 04:06:32 -0300

    add webpack-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,23 @@ const path = require('path');
 module.exports = {
     target: 'web',
     mode: 'development',
-
     entry: path.resolve(__dirname, 'src', 'main.js'),
-
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-    }
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        port: 3000,
+        open: true,
+        liveReload: true,
+    },
 }
+
+
 
 
 // na notação moderna do js
