c581464eab4762896ebd8e53c076d3341867bbf7
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-09-27 16:29:22 -0300

    add new schedule

---
+++ src/services/schedule-new.js
@@ -0,0 +1,22 @@
+import { apiConfig } from "./api-config.js";
+
+export async function scheduleNew({id, name,when}) {
+    try {
+        await fetch(`${apiConfig.baseURL}/schedules`, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                id,
+                name,
+                when
+            })
+        })
+
+        alert('Agendamento criado com sucesso!')
+    } catch (error) {
+        console.log(error);
+        alert('Não foi possível agendar. Tente novamente mais tarde!')
+    }
+}
