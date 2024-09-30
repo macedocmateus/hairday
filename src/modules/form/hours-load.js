3fd45e6a477c8cb977e3d2605f77c5b3ba6a7ce9
Mateus Macedo <mateusmacedoeng@gmail.com>
2024-09-30 05:11:38 -0300

    add block already scheduled times

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,10 +4,13 @@ import { hoursClick } from './hours-click.js';
 
 const hours = document.getElementById('hours')
 
-export function hoursLoad({date}) {
+export function hoursLoad({date, dailySchedules}) {
     // Limpa a lista de horários
     hours.innerHTML = ''
-    
+
+    // Obtém a lista de todos os horários ocupados
+    const unavailableHours = dailySchedules.map((schedule) => dayjs(schedule.when).format('HH:mm'))
+
     const opening = openingHours.map((hour) => {
         // Recupera somente a hora
         const [scheduleHour] = hour.split(':')
@@ -17,9 +20,11 @@ export function hoursLoad({date}) {
 
         const isHourPast = dayjs(date).add(scheduleHour, 'hour').isBefore(dayjs())
 
+        const available = !unavailableHours.includes(hour) && !isHourPast
+
         return ({
             hour,
-            available: !isHourPast,
+            available,
 
         })
         
